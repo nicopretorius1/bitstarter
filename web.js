d83adf8ba3f7e779081783080773afb2d1e70f57
Nico Pretorius <nico.pretorius@gmx.com>
2013-07-10 18:18:31 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+//  response.send('Hello World2!');
+  fs.writeFile('index.html', 'Hello Index', function(err) {
+    if (err) throw err;
+    console.log('It\'s saved!');
+  });
 });
 
 var port = process.env.PORT || 5000;
