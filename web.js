2224ce740ed06f17502c75016cf95963894c3e5d
Nico Pretorius <nico.pretorius@gmx.com>
2013-07-10 19:38:12 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,10 +5,8 @@ var fs = require('fs');
 
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
-  fs.writeFile('index.html', 'Hello Index', function(err) {
-    if (err) throw err;
-    console.log('It\'s saved!');
-  });
+  var k = fs.readFileSync('index.html');
+  response.send(k);
 });
 
 var port = process.env.PORT || 5000;
